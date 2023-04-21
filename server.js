69f5791e537ae954f1ee0384e7e8b663a0cbd006
mehdi ben hariz <benharizmehdi20@gmail.com>
2023-04-21 00:26:43 -0500

    fixed the server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const router = jsonServer.router("booky-db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 // Add this before server.use(router)
 server.use(
   // Add custom route here if needed
@@ -16,7 +17,7 @@ server.use(
 );
 server.use(router);
 // Listen to port
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("JSON Server is running");
 });
 
