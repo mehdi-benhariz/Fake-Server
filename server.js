fa4f848d1689dfaae4b900e730c48bb9c5b8578d
mehdi-benhariz <benharizmehdi20@gmail.com>
2023-10-30 21:55:48 +0100

    fixed bug

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 // Add this before server.use(router)
-server.use(
-  // Add custom route here if needed
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
+// server.use(
+//   // Add custom route here if needed
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
 server.use(router);
 // Listen to port
 server.listen(process.env.PORT || 3000, () => {
