ec8e2b187fbfc9b3740cfa1ce637c5d33f710d92
mehdi-benhariz <benharizmehdi20@gmail.com>
2023-10-30 22:17:33 +0100

    testing

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-//TODO : change this code so it uses different database based on URL
-// JSON Server module
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("mic-questions-db.json");
-
-// Make sure to use the default middleware
+const router = jsonServer.router('mic-questions-db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-// server.use(jsonServer.bodyParser);
-// Add this before server.use(router)
-// server.use(
-//   // Add custom route here if needed
-//   jsonServer.rewriter({
-//     "/api/*": "/$1",
-//   })
-// );
 server.use(router);
-// Listen to port
-server.listen(process.env.PORT || 3000, () => {
-  console.log("JSON Server is running");
-});
 
+server.listen(port);
