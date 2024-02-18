e55c47f09fbeb787feae4902e714ade5b0069ab8
mehdi-benhariz <benharizmehdi20@gmail.com>
2024-02-18 16:04:53 +0100

    added dynamic database chose

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("mic-questions-db.json");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/admin.html"));
+});
+// Function to create router dynamically based on the provided database file name
+function createRouter(databaseFileName) {
+  return jsonServer.router(databaseFileName);
+}
+
+// Read the database file name from "current-db.txt"
+let currentDBFileName = fs.readFileSync("current-db.txt", "utf8").trim();
+if (process.env.DB_NAME)
+  currentDBFileName = `${process.env.DB_NAME}.json`;
+
+let router = createRouter(currentDBFileName);
+
+
+
 // Add this before server.use(router)
 server.use(
   // Add custom route here if needed
@@ -15,11 +36,42 @@ server.use(
     "/api/*": "/$1",
   })
 );
+server.get("/list-databases", (req, res) => {
+  fs.readdir(__dirname, (err, files) => {
+    if (err)
+      res.status(500).send("Error listing databases");
+    else {
+      const jsonFiles = files.filter(file => file.endsWith("-db.json"));
+      res.json(jsonFiles);
+    }
+  });
+});
+
+//TODO find a solution for instant update of the database
+server.post("/update-database", (req, res) => {
+  const newFile = req.body.fileName;
+
+  // Update the value inside the "current-db.txt" file with the new database file name
+  fs.writeFile("current-db.txt", newFile, (err) => {
+    if (err) {
+      res.status(500).send("Error updating current-db.txt");
+    } else {
+
+      currentDBFileName = newFile;
+      router = createRouter(currentDBFileName); // Update router dynamically
+      server.use(router);
+
+      res.status(200).send(`Now serving ${newFile}`);
+
+    }
+  });
+});
 server.use(router);
 // Listen to port
-server.listen(process.env.PORT || 3000, () => {
+let listener = server.listen(process.env.PORT || 3000, () => {
   console.log("JSON Server is running");
 });
+
 
 // Export the Server API
 module.exports = server;
